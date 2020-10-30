89d7807065b58121610eed35c415809018c1939f
tony <tony@tonydeMacBook-Pro.local>
2020-10-30 18:01:30 +0800

    user info page undown

---
+++ lib/screens/userPage/loginScreen.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+
+}
---
+++ lib/screens/userPage/widgets/buyListEvent.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BuyList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text('1111最強購物')
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+
+          ],
+        ),
+      ],
+    );
+  }
+}
